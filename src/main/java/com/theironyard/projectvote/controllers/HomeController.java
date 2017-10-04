7ffb4cda5ac74de94fbcbd41c20aa68f20ba72c6
Curtis Schlak <curtis.schlak@theironyard.com>
2017-10-04 12:39:58 -0700

    Add mustache report.

--- src/main/java/com/theironyard/projectvote/controllers/HomeController.java
+++ src/main/java/com/theironyard/projectvote/controllers/HomeController.java
@@ -1,16 +1,33 @@
 package com.theironyard.projectvote.controllers;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.theironyard.projectvote.repositories.StudentRepository;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private StudentRepository students;
+	
+	public HomeController(StudentRepository students) {
+		this.students = students;
+	}
 
 	@GetMapping("")
 	public String index() {
 		return "redirect:/index.html";
+	}
+	
+	@GetMapping("votes")
+	public ModelAndView reports() {
+		ModelAndView mv = new ModelAndView("reports/reports");
+		mv.addObject("students", students.findAll(new Sort("lastName", "firstName")));
+		return mv;
 	}
 	
 }
